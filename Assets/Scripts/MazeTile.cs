4398736a5a5d69b4125add4c3d5020fda76a558e
Gustavo Andrade <gustavo.gustaandrade@gmail.com>
2020-06-25 17:30:31 -0300

    feat: makes maze assembler

---
+++ Assets/Scripts/MazeTile.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeTile : MonoBehaviour
+{
+    [Space(10), Header("Maze Tile Information")]
+    [SerializeField] private MazeTileType _mazeTileType;
+    public int TileX;
+    public int TileY;
+
+    [Space(10), Header("Maze Tile Neighbors")]
+    public MazeTile UpNeighbor;
+    public MazeTile LeftNeighbor;
+    public MazeTile DownNeighbor;
+    public MazeTile RightNeighbor;
+
+    private void Awake()
+    {
+        _mazeTileType = (MazeTileType)Enum.Parse(typeof(MazeTileType), gameObject.tag);
+    }
+
+    public void SetTilePosition(float posX, float posY)
+    {
+        TileX = (int)posX;
+        TileY = (int)posY;
+    }
+
+    public void SetNeighbors(MazeTile up, MazeTile left, MazeTile down, MazeTile right)
+    {
+        UpNeighbor = up;
+        LeftNeighbor = left;
+        DownNeighbor = down;
+        RightNeighbor = right;
+    }
+}
+
+[Serializable]
+public enum MazeTileType
+{
+    Wall,
+    Pellet,
+    Energizer,
+    Intersection,
+    OutOfBound,
+    Empty,
+    GhostHouse,
+    Tunnel,
+    Fruit
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform PlayerTransform;
+
+    private Animator _playerAnimator;
+
+    private void Awake()
+    {
+        _playerAnimator = PlayerTransform.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0.01f)
+        {
+            _playerAnimator.speed = 1f;
+            PlayerTransform.localScale = new Vector3(1f, 1f, 1f);
+            PlayerTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (Input.GetAxis("Horizontal") < -0.01f)
+        {
+            _playerAnimator.speed = 1f;
+            PlayerTransform.localScale = new Vector3(-1f, 1f, 1f);
+            PlayerTransform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (Input.GetAxis("Vertical") > 0.01f)
+        {
+            _playerAnimator.speed = 1f;
+            PlayerTransform.localScale = new Vector3(1f, 1f, 1f);
+            PlayerTransform.localRotation = Quaternion.Euler(0f, 0f, 90f);
+        }
+        else if (Input.GetAxis("Vertical") < -0.01f)
+        {
+            _playerAnimator.speed = 1f;
+            PlayerTransform.localScale = new Vector3(1f, 1f, 1f);
+            PlayerTransform.localRotation = Quaternion.Euler(0f, 0f, 270f);
+        }
+        else
+        {
+            _playerAnimator.speed = 0f;
+        }
+    }
+}
